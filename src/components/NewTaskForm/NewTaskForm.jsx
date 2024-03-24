0331183ecada7db76f91f3140aa36b6d188b2b64
Maxim Valishevskiy <mvalishevskiy@only.com.ru>
2024-03-24 23:51:48 +0700

    1

--- src/components/NewTaskForm/NewTaskForm.jsx
+++ src/components/NewTaskForm/NewTaskForm.jsx
@@ -6,7 +6,13 @@ export default function NewTaskForm({addTask}) {
 
     return (
         // eslint-disable-next-line jsx-a11y/no-autofocus
+
+    <form className="new-todo-form">
         <input className="new-todo" placeholder="What needs to be done?" autoFocus onKeyDown={addTask}/>
+            <input className="new-todo-form__timer" placeholder="Min" autoFocus/>
+                <input className="new-todo-form__timer" placeholder="Sec" autoFocus/>
+    </form>
+
     )
 }
 
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,40 +1,72 @@
 import React from "react";
 import './Task.css'
 import PropTypes from "prop-types";
-import {formatDistanceToNow} from "date-fns";
+import {format, formatDistanceToNow} from "date-fns";
 
 
 class Task extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            editing: false,
+            editText: '', // новое свойство для хранения текста задачи при редактировании
+        };
         this.changeTaskStatus = this.changeTaskStatus.bind(this);
+        this.handleEditTask = this.handleEditTask.bind(this);
+
+
     }
-    
+
     changeTaskStatus() {
-        const { task, tasks, setTasks } = this.props;
+        const {task, tasks, setTasks} = this.props;
         setTasks(tasks.map(item => task.id === item.id ? {...task, completed: !task.completed} : item));
     }
 
+    handleEditTask(e, id) {
+        const {editTask} = this.props;
+        if (editTask(e, id)) {
+            this.setState({editing: false})
+        }
+    }
+
+    handleswitchTimer(id, state) {
+        const {switchTimer, task} = this.props;
+        const {isRunning} = task;
+        if (state === isRunning) {
+            switchTimer(id)
+        }
+
+    }
+
     render() {
-        const { task, onDelete } = this.props;
-        const { text, completed = false, id , added} = task;
-        const status = completed ? "completed" : "";
+        const {task, onDelete} = this.props;
+        const {text, completed = false, id, added} = task;
+        const status = this.state.editing ? "editing" : completed ? "completed" : "";
+
         return (
             <li className={status}>
                 <div className="view">
-                    <input className="toggle" type="checkbox" checked={completed} onChange={this.changeTaskStatus} id={`taskToggle${  task.id}`} />
-                    <label htmlFor={`taskToggle${  task.id}`}>
-                        <span className="description">{text}</span>
-                        <span className="created">created {formatDistanceToNow(added, {includeSeconds: true})} ago</span>
+                    <input className="toggle" type="checkbox" checked={completed} onChange={this.changeTaskStatus}
+                           id={`taskToggle${task.id}`}/>
+                    <label htmlFor={`taskToggle${task.id}`}>
+                        <span className="title">{text}</span>
+                        <span className="description">
+                  <button className="icon icon-play" onClick={() => this.handleswitchTimer(task.id, false)}></button>
+                  <button className="icon icon-pause" onClick={() => this.handleswitchTimer(task.id, true)}></button>
+                            {format(task.time, 'mm:ss')}
+                </span>
+                        <span
+                            className="description">created {formatDistanceToNow(added, {includeSeconds: true})} ago</span>
                     </label>
-                    <button className="icon icon-edit" type="button" aria-label="Edit task">
-                        <span aria-hidden="true" />
+                    <button className="icon icon-edit" type="button" aria-label="Edit task" onClick={() => {this.setState({editing: true, editText: task.text})}}>
+                        <span aria-hidden="true"/>
                     </button>
-                    <button className="icon icon-destroy" type="button" onClick={() => onDelete(id)} aria-label="Delete task">
-                        <span aria-hidden="true" />
+                    <button className="icon icon-destroy" type="button" onClick={() => onDelete(id)}
+                            aria-label="Delete task">
+                        <span aria-hidden="true"/>
                     </button>
                 </div>
-                <input type="text" className="edit"/>
+                <input type="text" className="edit" value={this.state.editText} onChange={(e) => this.setState({editText: e.target.value})} onKeyDown={(e) => this.handleEditTask(e, task.id)}/>
             </li>
         );
     }
